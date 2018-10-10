e1a6413043275356c00fe5022a44104ed29a7e4f
ThisuriBandaranayake <thisu1995@gmail.com>
2018-10-09 23:29:30 -0700

    Connected apis to forms.

--- src/app/admin-sign-up/admin-sign-up.component.ts
+++ src/app/admin-sign-up/admin-sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-sign-up',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSignUpComponent implements OnInit {
 
-  constructor() { }
+  public form ={
+    username:null,
+    email:null,
+    password:null,
+    cpassword:null,
+  }
+  users;
+  id:number;
+  username:string;
+    email:string;
+    password:string;
+    cpassword:string;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }
+
+  onSubmit() {
+ 
+    let input = new FormData();
+    input.append('name',this.username);
+    input.append('email',this.email);
+    input.append('password',this.password);
+    input.append('confirm_password',this.cpassword);
+      return this.http.post('http://127.0.0.1:8000/api/admin',input).subscribe(
+        data => {
+          this.users = data;
+          console.log(data);
+          this.username=null;
+          this.email=null;
+          this.password=null;
+          this.cpassword=null;
+        },
+    error => console.log(error)
+      );
+     
+    } 
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+ public form={
+    email:null,
+    password:null,
+  }
+
+  users;
+    id:number;
+    email:string;
+    password:string;
+
   elegantForm: FormGroup;
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder,private http:HttpClient) {
     this.elegantForm = fb.group({
       elegantFormEmailEx: ['', [Validators.required, Validators.email]],
       elegantFormPasswordEx: ['', Validators.required],
@@ -15,6 +26,20 @@ export class LoginComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+   let  input=new FormData;
+   input.append('email',this.email);
+   input.append('password',this.password);
+   return this.http.post('http://127.0.0.1:8000/api/login',input).subscribe(
+     data=>{
+       this.users=data;
+       console.log(data);
+       this.email=null;
+       this.password=null;
+     }
+   )
   }
 
 }
