9b7da564bf24ca25a024066101bbd36f1a476cfc
ThisuriBandaranayake <thisu1995@gmail.com>
2018-10-21 22:47:48 -0700

    user sign up modified

--- src/app/user-sign-up/user-sign-up.component.ts
+++ src/app/user-sign-up/user-sign-up.component.ts
@@ -13,6 +13,11 @@ export class UserSignUpComponent implements OnInit {
     email:null,
     password:null,
     cpassword:null,
+    fname:null,
+    lname:null,
+    gender:null,
+    contactno:null,
+    birthday:null,
   }
   users;
   id:number;
@@ -20,6 +25,12 @@ export class UserSignUpComponent implements OnInit {
     email:string;
     password:string;
     cpassword:string;
+    fname:string;
+    lname:string;
+    gender:string;
+    contactno:string;
+    birthday:string;
+
   constructor(private http:HttpClient) { }
 
   ngOnInit() {
@@ -45,5 +56,37 @@ export class UserSignUpComponent implements OnInit {
     );
    
   } 
+
+  signup () {
+ 
+    let input = new FormData();
+    input.append('name',this.username);
+    input.append('email',this.email);
+    input.append('first_name',this.fname);
+    input.append('last_name',this.lname);
+    input.append('phone_number',this.contactno);
+    input.append('birthday',this.birthday);
+    input.append('gender',this.gender);
+    input.append('user_type',"customer");
+    input.append('password',this.password);
+    input.append('confirm_password',this.cpassword);
+      return this.http.post('http://127.0.0.1:8000/api/user/signup',input).subscribe(
+        data => {
+          this.users = data;
+          console.log(data);
+          this.username=null;
+          this.email=null;
+          this.password=null;
+          this.cpassword=null;
+          this.fname=null;
+          this.lname=null;
+          this.contactno=null;
+          this.birthday=null;
+          this.gender=null;
+        },
+    error => console.log(error)
+      );
+     
+    } 
   
 }
