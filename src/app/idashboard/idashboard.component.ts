4fc777c8c062caaa07177b64ccaec3043782f935
ThisuriBandaranayake <thisu1995@gmail.com>
2018-10-23 16:52:10 -0700

    directed relevently

--- src/app/idashboard/idashboard.component.ts
+++ src/app/idashboard/idashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../auth.service';
 @Component({
   selector: 'app-idashboard',
   templateUrl: './idashboard.component.html',
   styleUrls: ['./idashboard.component.scss']
 })
 export class IDashboardComponent implements OnInit {
+  user;
+  institutes
 
-  constructor() { }
+  constructor(private auth:AuthService) { }
 
   ngOnInit() {
+    this.auth
+    .getUserDetails(localStorage.getItem("access_token"))
+      .subscribe(response => {
+       
+        if(response['body']['user_type']=="institute"){
+          console.log(response)
+          }
+       
+      });
   }
 
 }
